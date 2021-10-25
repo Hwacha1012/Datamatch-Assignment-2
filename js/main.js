6b9a3068e433d97574b60b45ef519c11421113dc
Leo Liu <leoqh6@gmail.com>
2021-10-24 20:45:00 -0400

    Finished up to Step 7

--- js/main.js
+++ js/main.js
@@ -43,7 +43,7 @@ var createVisualization = function(data) {
 	// Step 2: Append a new SVG area with D3
 	// The ID of the target div container in the html file is #chart-area
 	// Use the margin convention with 50 px of bottom padding and 30 px of padding on other sides!
-	var margin= {top: 30, right: 30, left: 30, bottom: 50};
+	var margin = {top: 30, right: 30, left: 30, bottom: 50};
 	d3.select("#chart-area").append("svg")
 			.attr("width", width + margin.left + margin.right)
 			.attr("height", height + margin.top + margin.bottom)
@@ -56,35 +56,59 @@ var createVisualization = function(data) {
 	// Call them numSongsScale and streamsScale.
 	// Use d3.min() and d3.max() for the input domain
 	// Use the variables height and width for the output range
-
+	var padding = 20;
 	var numSongsScale = d3.scaleLinear()
-		.domain([0, d3.max(data, function(d) { return d[0]; })])
-		.range([0, width]);
+		.domain([0, d3.max(data, function(d) { return d.songs; })])
+		.range([padding, width-padding]);
 	var streamsScale = d3.scaleLinear()
-		.domain([0, d3.max(data, function(d) { return d[1]; })])
-		.range([0, height]);
+		.domain([0, d3.max(data, function(d) { return d.streams_in_mils; })])
+		.range([padding, height-padding]);
 
 	// Step 4: Try the scale functions
 	// You can call the functions with example values and print the result to the web console.
-	console.log(numSongsScale(0));
+	console.log(numSongsScale(1));
 	console.log(numSongsScale(100));
-	console.log(streamsScale(0));
+	console.log(numSongsScale(1000));
+	console.log(streamsScale(1));
 	console.log(streamsScale(100));
-
+	console.log(streamsScale(1000));
 
 	// Step 5: Map the countries to SVG circles
 	// Use select(), data(), enter() and append()
 	// Instead of setting the x- and y-values directly,
 	// you have to use your scale functions to convert the data values to pixel measures
-
+	d3.select("#chart-area").select("svg").selectAll("circle")
+		.data(data)
+		.enter()
+		.append("circle")
+		.attr("r", 10)
+		.attr("fill", "black")
+		.attr("cx", function (d){
+			return numSongsScale(d.songs)
+		})
+		.attr("cy", function (d){
+			return streamsScale(d.streams_in_mils)
+		});
 
 	// Step 6: Use your scales (songs and streams) to create D3 axis functions
-
+	var xAxis = d3.axisBottom().scale(numSongsScale);
+	var yAxis = d3.axisLeft().scale(streamsScale);
 
 	// Step 7: Append the x- and y-axis to your scatterplot
 	// Add the axes to a group element that you add to the SVG drawing area.
 	// Use translate() to change the axis position
-
+	d3.select("#chart-area").select("svg")
+		.append("g")
+		.attr("class", "xAxis")
+		.attr("transform", "translate(0," + (padding) + ")")
+		.call(xAxis);
+	d3.select("#chart-area").select("svg")
+		.append("g")
+		.attr("class", "yAxis")
+		.attr("transform", "translate("+(width-padding)+",0)")
+		//.attr("transform", "translate("+(padding)+",)")
+		.call(yAxis);
+	//.call(yAxis);
 
 	// Step 8: Refine the domain of the scales
 	// Notice that some of the circles are positioned on the outer edges of the svg area
